6ec70c723247904a947e5797d6a69856dbe6d1c7
Deray <deraygo@gmail.com>
2016-03-01 16:36:15 +0800

    fixed download link

--- js/__original.js
+++ js/__original.js
@@ -1,3 +1,20 @@
+var YouTubeParser, getYouTubeUrl;
+
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function(needle) {
+    var i, item, j, len, pos;
+    pos = -1;
+    for (i = j = 0, len = this.length; j < len; i = ++j) {
+      item = this[i];
+      if (item === needle) {
+        pos = i;
+      }
+      break;
+    }
+    return pos;
+  };
+}
+
 YouTubeParser = {
   videoInfo: '',
   rdata: [],
@@ -25,53 +42,42 @@ YouTubeParser = {
     100: '360p&nbsp;&nbsp;&nbsp;(WebM(VP8), 640 x 360, Stereo 44KHz Vorbis)',
     101: '480p&nbsp;&nbsp;&nbsp;(WebM(VP8), 854 x 480, Stereo 44KHz Vorbis)',
     102: '720p&nbsp;&nbsp;&nbsp;(WebM(VP8), 1280 x 720, Stereo 44KHz Vorbis)',
-    133: '240p&nbsp;&nbsp;&nbsp;(MP4(H.264), 426 x 240, <span">video only, no audio</span>)',
-    134: '360p&nbsp;&nbsp;&nbsp;(MP4(H.264), 640 x 360, <span">video only, no audio</span>)',
-    135: '480p&nbsp;&nbsp;&nbsp;(MP4(H.264), 854 x 480, <span">video only, no audio</span>)',
-    136: '720p&nbsp;&nbsp;&nbsp;(MP4(H.264), 1280 x 720, <span">video only, no audio</span>)',
-    137: '1080p&nbsp;&nbsp;&nbsp;(MP4(H.264), 1920 x 1080, <span">video only, no audio</span>)',
-    138: '3072p&nbsp;&nbsp;&nbsp;(MP4(H.264), 4096 x 3072, <span">video only, no audio</span>)',
-    160: '144p&nbsp;&nbsp;&nbsp;(MP4(H.264), 256 x 144, <span">video only, no audio</span>)',
-    264: '1440p&nbsp;&nbsp;&nbsp;(MP4(H.264), 2560 x 1440, <span">video only, no audio</span>)',
-    266: '2160p&nbsp;&nbsp;&nbsp;(MP4(H.264), 3840 x 2160, <span">video only, no audio</span>)',
-    298: '720p HFR&nbsp;&nbsp;&nbsp;(MP4(H.264), 1280 x 720 HFR, <span">video only, no audio</span>)',
-    299: '1080p HFR&nbsp;&nbsp;&nbsp;(MP4(H.264), 1920 x 1080 HFR, <span">video only, no audio</span>)',
-    242: '240p&nbsp;&nbsp;&nbsp;(WebM(VP9), 426 x 240, <span">video only, no audio</span>)',
-    243: '360p&nbsp;&nbsp;&nbsp;(WebM(VP9), 640 x 360, <span">video only, no audio</span>)',
-    244: '480p&nbsp;&nbsp;&nbsp;(WebM(VP9), 854 x 480, <span">video only, no audio</span>)',
-    247: '720p&nbsp;&nbsp;&nbsp;(WebM(VP9), 1280 x 720, <span">video only, no audio</span>)',
-    248: '1080p&nbsp;&nbsp;&nbsp;(WebM(VP9), 1920 x 1080, <span">video only, no audio</span>)',
-    271: '1440p&nbsp;&nbsp;&nbsp;(WebM(VP9), 2560 x 1440, <span">video only, no audio</span>)',
-    272: '2160p&nbsp;&nbsp;&nbsp;(WebM(VP9), 3840 x 2160, <span">video only, no audio</span>)',
-    278: '144p&nbsp;&nbsp;&nbsp;(WebM(VP9), 256 x 144, <span">video only, no audio</span>)',
-    302: '720p HFR&nbsp;&nbsp;&nbsp;(WebM(VP9), 1280 x 720 HFR, <span">video only, no audio</span>)',
-    303: '1080p HFR&nbsp;&nbsp;&nbsp;(WebM(VP9), 1920 x 1080 HFR, <span">video only, no audio</span>)',
-    308: '1440p HFR&nbsp;&nbsp;&nbsp;(WebM(VP9), 2560 x 1440 HFR, <span">video only, no audio</span>)',
-    313: '2160&nbsp;&nbsp;&nbsp;(WebM(VP9), 3840 x 2160, <span">video only, no audio</span>)',
-    315: '2160p HFR&nbsp;&nbsp;&nbsp;(WebM(VP9), 3840 x 2160 HFR, <span">video only, no audio</span>)',
-    139: '(M4A(AAC), 48 kbit/s <span">audio only</span>)',
-    140: '(M4A(AAC), 128 kbit/s <span">audio only</span>)',
-    141: '(M4A(AAC), 256 kbit/s <span">audio only</span>)',
-    171: '(WebM(Vorbis), 128 kbit/s <span">audio only</span>)',
-    172: '(WebM(Vorbis), 192 kbit/s <span">audio only</span>)'
+    133: '240p&nbsp;&nbsp;&nbsp;(MP4(H.264), 426 x 240, <span style="color:#f00;">video only, no audio</span>)',
+    134: '360p&nbsp;&nbsp;&nbsp;(MP4(H.264), 640 x 360, <span style="color:#f00;">video only, no audio</span>)',
+    135: '480p&nbsp;&nbsp;&nbsp;(MP4(H.264), 854 x 480, <span style="color:#f00;">video only, no audio</span>)',
+    136: '720p&nbsp;&nbsp;&nbsp;(MP4(H.264), 1280 x 720, <span style="color:#f00;">video only, no audio</span>)',
+    137: '1080p&nbsp;&nbsp;&nbsp;(MP4(H.264), 1920 x 1080, <span style="color:#f00;">video only, no audio</span>)',
+    138: '3072p&nbsp;&nbsp;&nbsp;(MP4(H.264), 4096 x 3072, <span style="color:#f00;">video only, no audio</span>)',
+    160: '144p&nbsp;&nbsp;&nbsp;(MP4(H.264), 256 x 144, <span style="color:#f00;">video only, no audio</span>)',
+    264: '1440p&nbsp;&nbsp;&nbsp;(MP4(H.264), 2560 x 1440, <span style="color:#f00;">video only, no audio</span>)',
+    266: '2160p&nbsp;&nbsp;&nbsp;(MP4(H.264), 3840 x 2160, <span style="color:#f00;">video only, no audio</span>)',
+    298: '720p HFR&nbsp;&nbsp;&nbsp;(MP4(H.264), 1280 x 720 HFR, <span style="color:#f00;">video only, no audio</span>)',
+    299: '1080p HFR&nbsp;&nbsp;&nbsp;(MP4(H.264), 1920 x 1080 HFR, <span style="color:#f00;">video only, no audio</span>)',
+    242: '240p&nbsp;&nbsp;&nbsp;(WebM(VP9), 426 x 240, <span style="color:#f00;">video only, no audio</span>)',
+    243: '360p&nbsp;&nbsp;&nbsp;(WebM(VP9), 640 x 360, <span style="color:#f00;">video only, no audio</span>)',
+    244: '480p&nbsp;&nbsp;&nbsp;(WebM(VP9), 854 x 480, <span style="color:#f00;">video only, no audio</span>)',
+    247: '720p&nbsp;&nbsp;&nbsp;(WebM(VP9), 1280 x 720, <span style="color:#f00;">video only, no audio</span>)',
+    248: '1080p&nbsp;&nbsp;&nbsp;(WebM(VP9), 1920 x 1080, <span style="color:#f00;">video only, no audio</span>)',
+    271: '1440p&nbsp;&nbsp;&nbsp;(WebM(VP9), 2560 x 1440, <span style="color:#f00;">video only, no audio</span>)',
+    272: '2160p&nbsp;&nbsp;&nbsp;(WebM(VP9), 3840 x 2160, <span style="color:#f00;">video only, no audio</span>)',
+    278: '144p&nbsp;&nbsp;&nbsp;(WebM(VP9), 256 x 144, <span style="color:#f00;">video only, no audio</span>)',
+    302: '720p HFR&nbsp;&nbsp;&nbsp;(WebM(VP9), 1280 x 720 HFR, <span style="color:#f00;">video only, no audio</span>)',
+    303: '1080p HFR&nbsp;&nbsp;&nbsp;(WebM(VP9), 1920 x 1080 HFR, <span style="color:#f00;">video only, no audio</span>)',
+    308: '1440p HFR&nbsp;&nbsp;&nbsp;(WebM(VP9), 2560 x 1440 HFR, <span style="color:#f00;">video only, no audio</span>)',
+    313: '2160&nbsp;&nbsp;&nbsp;(WebM(VP9), 3840 x 2160, <span style="color:#f00;">video only, no audio</span>)',
+    315: '2160p HFR&nbsp;&nbsp;&nbsp;(WebM(VP9), 3840 x 2160 HFR, <span style="color:#f00;">video only, no audio</span>)',
+    139: '(M4A(AAC), 48 kbit/s <span style="color:#f00;">audio only</span>)',
+    140: '(M4A(AAC), 128 kbit/s <span style="color:#f00;">audio only</span>)',
+    141: '(M4A(AAC), 256 kbit/s <span style="color:#f00;">audio only</span>)',
+    171: '(WebM(Vorbis), 128 kbit/s <span style="color:#f00;">audio only</span>)',
+    172: '(WebM(Vorbis), 192 kbit/s <span style="color:#f00;">audio only</span>)'
   },
-
+  setVideoInfo: function(videoInfo) {
+    this.videoInfo = videoInfo;
+  },
   buildVideoUrlHTMLTag: function(item, title, method) {
-    var link = this.fmt_str[item.fmt];
-    link = link.split(',');
-    var format = link[0].replace('(', '');
-    link.shift();
-    var dl = unescape(item.fmt_url);
-
-    if(item.fmt_sig)
-      dl += ('&signature=' + item.fmt_sig );
-    dl += '&title=' + escape(title.replace('"', ''));
-
-    return '<a href="' + dl + '"' + ' download="' + dl + '" ">' +
-      '<div class="dl"><i class="fa fa-download" /> ' + method + '&nbsp;&nbsp;&nbsp;' + format + '</div>' +
-      ((link.length) ? ('<div class="desc">(' + link.join(',') + '</div>') : '') + '</a>';
+    return '<a href="' + unescape(item.fmt_url) + (item.fmt_sig === false ? "" : "&signature=" + item.fmt_sig) + "&title=" + escape(title.replace('"', '')) + '" target="_blank"><b>' + method + '&nbsp;&nbsp;&nbsp;' + this.fmt_str[item.fmt] + '</b></a>';
   },
-
   parseInfo: function(infostr) {
     var item, j, len, result, tmp, tmp2;
     result = {};
@@ -83,10 +89,7 @@ YouTubeParser = {
     }
     return result;
   },
-
-  getYouTubeUrl: function(videoInfo) {
-    this.videoInfo = videoInfo;
-
+  getYouTubeUrl: function() {
     var div_dl, dllinks, dllinksAdaptive, dllinksAlter, item, j, k, l, len, len1, len2, rdataArray, rdata_reason, rdata_status, result, succ, title, url_adaptive, url_alter, url_classic, webmlinks;
     succ = 0;
     dllinks = '';
@@ -99,72 +102,72 @@ YouTubeParser = {
     url_adaptive = this.parseUrlsAdaptive(rdataArray);
     url_alter = this.parseUrlsAlter(rdataArray, url_classic, url_adaptive);
     title = this.parseTitle(rdataArray);
-    var downloadTxt = chrome.i18n.getMessage("download");
-
     for (j = 0, len = url_classic.length; j < len; j++) {
       item = url_classic[j];
       if ([43, 44, 45, 46, 100, 101, 102].indexOf(parseInt(item.fmt, 10)) > -1) {
-        /*if (webmlinks.length > 0) {
+        if (webmlinks.length > 0) {
           webmlinks += '<br />';
         }
-        webmlinks += this.buildVideoUrlHTMLTag(item, title, 'Watch online');*/
+        webmlinks += this.buildVideoUrlHTMLTag(item, title, 'Watch online');
       } else {
-        var vlink = this.buildVideoUrlHTMLTag(item, title, downloadTxt);
-
-        if ([18, 22, 37, 38, 82, 83, 84, 85].indexOf(parseInt(item.fmt, 10)) > -1) {
-          if (dllinks.length > 0) {
-            dllinks += '<hr />';
-          }
-          dllinks += vlink;
-        } else {
-          if (dllinksAdaptive.length > 0) {
-            //dllinksAdaptive += '<br />';
-          }
-          dllinksAdaptive += vlink;
+        if (dllinks.length > 0) {
+          dllinks += '<br />';
         }
+        dllinks += this.buildVideoUrlHTMLTag(item, title, 'Download');
       }
     }
-
-    /*if (webmlinks.length > 0) {
+    if (webmlinks.length > 0) {
       if (dllinks.length > 0) {
         dllinks += '<br />';
       }
       dllinks += webmlinks;
-    }*/
-
-    /*if (url_alter.length > 0) {
+    }
+    if (url_alter.length > 0) {
       for (k = 0, len1 = url_alter.length; k < len1; k++) {
         item = url_alter[k];
-        dllinksAlter += this.buildVideoUrlHTMLTag(item, title, downloadTxt);
+        if (dllinksAlter.length > 0) {
+          dllinksAlter += '<br />';
+        }
+        dllinksAlter += this.buildVideoUrlHTMLTag(item, title, 'Download');
       }
-    }*/
-    /*if (dllinksAlter.length > 0) {
-      dllinks += '<br /><br /><span font-weight:bold;">sadly 1080p\'s dead again...</span><br /><del>1080p & some other formats redirect download are back online and <spanfont-weight:bold;">testing</span>:<br />';
+    }
+    if (dllinksAlter.length > 0) {
+      dllinks += '<br /><br /><span style="color:#f00; font-weight:bold;">sadly 1080p\'s dead again...</span><br /><del>1080p & some other formats redirect download are back online and <span style="color:#f00;font-weight:bold;">testing</span>:<br />';
       dllinks += dllinksAlter + '</del>';
-    }*/
+    }
     for (l = 0, len2 = url_adaptive.length; l < len2; l++) {
       item = url_adaptive[l];
       if (dllinksAdaptive.length > 0) {
-        //dllinksAdaptive += '<br />';
+        dllinksAdaptive += '<br />';
       }
-      dllinksAdaptive += this.buildVideoUrlHTMLTag(item, title, downloadTxt);
+      dllinksAdaptive += this.buildVideoUrlHTMLTag(item, title, 'Download');
     }
     if (dllinksAdaptive.length > 0) {
       if (dllinks.length > 0) {
-        dllinks += '<hr /><div class="small">';
+        dllinks += '<br /><br />special files (separated audio and video):<br />';
       }
       dllinks += dllinksAdaptive;
-      dllinks += '</div>';
     }
-
     if (dllinks.length > 0) {
-      $('#downloadInfo').addClass('wide');
-      $('#downloadInfo').html(dllinks);
-    } else {
-      videoNotFound();
+      $('#result_div').remove();
+      div_dl = document.createElement('div');
+      $(div_dl).html(dllinks).css('padding', '7px 0 0 0');
+      $(div_dl).attr('id', 'result_div');
+      $('#videoInfo').after(div_dl);
+      $('#downloadInfo').css('display', 'block');
+      succ = 1;
+    }
+    if (succ === 0) {
+      rdata_status = rdataArray['status'];
+      rdata_reason = this.urldecode(escape(rdataArray['reason']));
+      result = '<b>&#28961;&#27861;&#21462;&#24471;&#24433;&#29255; URL</b><br />status : <span style="color:#f00;">' + rdata_status + '</span><br />' + 'reason : <span style="color:#f00;">' + rdata_reason + '</span>';
+      $('#result_div').remove();
+      div_dl = document.createElement('div');
+      $(div_dl).html(result).css('padding', '7 0 7px 0');
+      $(div_dl).attr('id', 'result_div');
+      $('#videoInfo').after(div_dl);
     }
   },
-
   parseUrlsClassic: function(rdataArray) {
     var data, dataset, item, items, j, len, temp_type;
     items = [];
@@ -197,7 +200,6 @@ YouTubeParser = {
     }
     return items;
   },
-
   parseUrlsAdaptive: function(rdataArray) {
     var data, dataset, item, items, j, len, temp_type;
     items = [];
@@ -228,7 +230,6 @@ YouTubeParser = {
     }
     return items;
   },
-
   parseUrlsAlter: function(rdataArray, url_classic, url_adaptive) {
     var base_url, dataset, fmt_adaptive, fmt_classic, i, item35, item37, item38, items, j, k, l, len, len1, len2, len3, len4, m, n, qs, qstemp, temp, tempurl, tmp;
     items = [];
@@ -295,29 +296,50 @@ YouTubeParser = {
     }
     return [];
   },
-
   parseTitle: function(rdataArray) {
     if (typeof rdataArray.title !== "undefined") {
       return rdataArray.title.replace(/%22/g, '');
     }
     return '';
   },
-
   urldecode: function(str) {
+
+    /*
+    Decodes URL-encoded string  
+    
+    version: 1004.2314
+    discuss at: http://phpjs.org/functions/urldecode    // +   original by: Philip Peterson
+    +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    +      input by: AJ
+    +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    +   improved by: Brett Zamir (http://brett-zamir.me)    // +      input by: travc
+    +      input by: Brett Zamir (http://brett-zamir.me)
+    +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    +   improved by: Lars Fischer
+    +      input by: Ratheous    // +   improved by: Orlando
+    +      reimplemented by: Brett Zamir (http://brett-zamir.me)
+    +      bugfixed by: Rob
+    %        note 1: info on what encoding functions to use from: http://xkr.us/articles/javascript/encode-compare/
+    %        note 2: Please be aware that this function expects to decode from UTF-8 encoded strings, as found on    // %        note 2: pages served as UTF-8
+    *     example 1: urldecode('Kevin+van+Zonneveld%21');
+    *     returns 1: 'Kevin van Zonneveld!'
+    *     example 2: urldecode('http%3A%2F%2Fkevin.vanzonneveld.net%2F');
+    *     returns 2: 'http://kevin.vanzonneveld.net/'
+    *     example 3: urldecode('http%3A%2F%2Fwww.google.nl%2Fsearch%3Fq%3Dphp.js%26ie%3Dutf-8%26oe%3Dutf-8%26aq%3Dt%26rls%3Dcom.ubuntu%3Aen-US%3Aunofficial%26client%3Dfirefox-a');
+    *     returns 3: 'http://www.google.nl/search?q=php.js&ie=utf-8&oe=utf-8&aq=t&rls=com.ubuntu:en-US:unofficial&client=firefox-a'
+     */
     return decodeURIComponent(str.replace(/\+/g, '%20'));
   },
-
   swap: function(sArray, location) {
     var ref;
     location = location % sArray.length;
     ref = [sArray[location], sArray[0]], sArray[0] = ref[0], sArray[location] = ref[1];
     return sArray;
   },
-
   SigHandlerAlternative: function(s) {
     var code, j, len, sArray, scode;
     sArray = s.split("");
-    scode = [4, 3, 0, 50, 0, -1, 20, -1];
+    scode = [54, 0, -1, 52, -3, 0, 16, 0];
     for (j = 0, len = scode.length; j < len; j++) {
       code = scode[j];
       if (code > 0) {
@@ -332,42 +354,7 @@ YouTubeParser = {
   }
 };
 
-function videoNotFound() {
-  var videoNotFound = chrome.i18n.getMessage("videoNotFound");
-  $('#downloadInfo').html(videoNotFound).addClass('errormsg');
-}
-
-function getQueryVariable(variable, query) {
-  if(!query)
-    return undefined;
-
-  var vars = query.split("&");
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split("=");
-    if (pair[0] == variable) {
-      return pair[1];
-    }
-  }
-  return undefined;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  chrome.tabs.getSelected(null, function(tab) {
-    var tablink = tab.url;
-    //1. get video id
-    //https://www.youtube.com/watch?v=hzbp4QgBgho
-    var video_id = getQueryVariable('v', tablink.split('?')[1]);
-
-    //2. using AJAX get video info via id
-    //http://www.youtube.com/get_video_info?eurl=http%3A%2F%2Fkej.tw%2F&sts=16849&video_id=hzbp4QgBgho
-    if (!video_id) {
-      videoNotFound();
-      return;
-    }
-
-    $.get('http://www.youtube.com/get_video_info?eurl=http%3A%2F%2Fkej.tw%2F&sts=16849&video_id=' + video_id, function(data) {
-      //3. Parse link for download
-      YouTubeParser.getYouTubeUrl(data);
-    });
-  });
-});
+getYouTubeUrl = function() {
+  YouTubeParser.setVideoInfo($('#videoInfo').val());
+  return YouTubeParser.getYouTubeUrl();
+};
